63422f0411ea96a1e8a7d5b216de82531d924201
Rikelvem Silva <rikelvemrsilva@gmail.com>
2022-01-24 17:55:33 -0300

    iniciando no react.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+    <div>
+        <h1>Que bom te ver por aqui!</h1>
+        <h2>Anime House - Dragon Cord</h2>
+    </div>
+    )
+}
+
+export default HomePage
